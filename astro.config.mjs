7a7a1e5fbc45be2ebd6f301f5f0834813c47aa24
Peter Bartha <peterbartha@pm.me>
2023-06-01 19:35:00 +0200

    build(vercel): prepare for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercelStatic from '@astrojs/vercel/static';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static',
+  adapter: vercelStatic(),
 });
