019917aad1648dd1e74d649765c1b683f3fa6d5e
Peter Bartha <peterbartha@pm.me>
2023-06-07 22:33:07 +0200

    feat(theme): add `Wotfard` and `Bitter` font subsets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      sans: ['BitterVariable', ...defaultTheme.fontFamily.sans],
+      serif: ['Wotfard', ...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
     extend: {},
   },
   plugins: [],
