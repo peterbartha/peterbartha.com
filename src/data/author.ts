cfdc7c94ff2cf011922ddb6641d3d66d7a8cb702
Peter Bartha <peterbartha@pm.me>
2023-06-10 12:43:46 +0200

    feat(seo): create a reusable SEO component for pages and posts

---
+++ src/data/author.ts
@@ -0,0 +1,12 @@
+/**
+ * 1. You are free to use this code as inspiration.
+ * 2. Please DO NOT copy it directly -- every site is beautiful when it's unique, hides little
+ *    surprises and stands out a little from the rest! :)
+ * 3. Remove all of my personal information (resume, blog posts, images, etc.).
+ * 4. Crediting the author is always welcomed and appreciated.
+ *
+ * Thank you for checking out my work.
+ * Be kind and help others learn and grow!
+ */
+export const author = 'Peter Bartha';
+export const twitterHandle = 'pbartha7';
