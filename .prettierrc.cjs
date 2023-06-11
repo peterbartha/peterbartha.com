7fc1d2adfe91ffc7a27208b3ce2cd1920a4c38ae
Peter Bartha <peterbartha@pm.me>
2023-06-11 16:43:57 +0200

    fix(lint): fix prettier's VSCode compatibility

---
+++ .prettierrc.cjs
@@ -0,0 +1,35 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  pluginSearchDirs: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+
+  // Recommended configuration for *.astro files
+  astroAllowShorthand: true,
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
