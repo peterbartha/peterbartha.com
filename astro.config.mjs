2515f86ee61785df1f658406eb6e8ef1e0bd2511
Peter Bartha <peterbartha@pm.me>
2023-06-11 15:58:29 +0200

    fix(seo): calculate canonical URL based on `Astro.site`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://peterbartha.com',
   integrations: [tailwind()],
   output: 'static',
   adapter: vercelStatic(),
