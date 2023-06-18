68687343b62907d92a49f594ce49123b4ef72233
Peter Bartha <peterbartha@pm.me>
2023-06-18 22:27:57 +0200

    feat(theme): define new animations for titles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,7 +23,23 @@ module.exports = {
         900: 'hsl(var(--color-indigo-darker) / <alpha-value>)',
       },
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        reveal: {
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+        },
+        underline: {
+          '0%': { opacity: 0, transform: 'translateX(500%) scaleX(7)' },
+          '1%': { opacity: 1 },
+          '70%': { transform: 'translateX(0) scaleX(.5)' },
+          to: { transform: 'translateX(0) scaleX(1)' },
+        },
+      },
+      animation: {
+        reveal: 'reveal .8s cubic-bezier(.22,.61,.36,1) .3s backwards',
+        underline: 'underline 1s cubic-bezier(0.25,0.1,0.5,1.27) 0.3s both',
+      },
+    },
   },
   plugins: [],
   future: {
