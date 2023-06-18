5fc1a13f11a51ff239dd636c6c31bf145b4968fd
Peter Bartha <peterbartha@pm.me>
2023-06-18 22:20:16 +0200

    feat(theme): define custom theme colors as variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -9,40 +10,20 @@ module.exports = {
       serif: ['Wotfard', ...defaultTheme.fontFamily.serif],
       mono: [...defaultTheme.fontFamily.mono],
     },
-    extend: {
-      colors: {
-        bermuda: {
-          50: 'hsl(158, 65%, 97%)',
-          100: 'hsl(161, 67%, 89%)', // bg
-          200: 'hsl(162, 66%, 78%)',
-          300: 'hsl(164, 61%, 65%)',
-          400: 'hsl(166, 53%, 50%)',
-          500: 'hsl(167, 64%, 40%)',
-          600: 'hsl(168, 66%, 32%)',
-          700: 'hsl(169, 61%, 26%)',
-          800: 'hsl(169, 55%, 22%)',
-          900: 'hsl(170, 48%, 19%)',
-          950: 'hsl(171, 69%, 10%)',
-        },
-        mint: {
-          50: 'hsl(172, 79%, 96%)',
-          100: 'hsl(172, 74%, 92%)',
-          200: 'hsl(173, 72%, 82%)',
-          300: 'hsl(174, 72%, 69%)', // action
-          400: 'hsl(175, 66%, 53%)',
-          500: 'hsl(175, 73%, 43%)',
-          600: 'hsl(179, 71%, 36%)',
-          700: 'hsl(179, 63%, 31%)',
-          800: 'hsl(181, 54%, 27%)',
-          900: 'hsl(183, 49%, 24%)',
-          950: 'hsl(184, 61%, 15%)',
-        },
-        indigo: {
-          800: 'hsl(231, 73%, 22%)',
-          900: 'hsl(232, 85%, 15%)',
-        },
+    colors: {
+      transparent: colors.transparent,
+      current: colors.current,
+      black: colors.black,
+      white: colors.white,
+      'white-ice': 'hsl(var(--color-white-ice) / <alpha-value>)',
+      bermuda: 'hsl(var(--color-bermuda) / <alpha-value>)',
+      mint: 'hsl(var(--color-mint) / <alpha-value>)',
+      indigo: {
+        800: 'hsl(var(--color-indigo) / <alpha-value>)',
+        900: 'hsl(var(--color-indigo-darker) / <alpha-value>)',
       },
     },
+    extend: {},
   },
   plugins: [],
   future: {
