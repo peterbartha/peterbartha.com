5b0343196361e5146bf6d948f05a4cbf1c883911
Peter Bartha <peterbartha@pm.me>
2023-06-19 23:56:51 +0200

    perf: configure `@astrojs/image` with `sharp` image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 import vercelStatic from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
@@ -5,7 +6,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://peterbartha.com',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
   output: 'static',
   adapter: vercelStatic(),
 });
