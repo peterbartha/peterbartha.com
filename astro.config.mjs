0415c179c7a0cba8be4758bae0fee04d01dba517
Peter Bartha <peterbartha@pm.me>
2024-04-02 22:54:17 +0200

    build(astro): upgrade to Astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 import vercelStatic from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
@@ -6,13 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://peterbartha.com',
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-  ],
+  integrations: [tailwind()],
   output: 'static',
-  compressHTML: true,
   adapter: vercelStatic(),
 });
